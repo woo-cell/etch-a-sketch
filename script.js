86c70b8fef71072fb27836596e8a2ce61e65c09e
woo <150315305+woo-cell@users.noreply.github.com>
2023-11-22 22:48:14 +0300

    added a slider for grid size and a clear button

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
-let numberSide = 16;
-let numberSquare = numberSide ** 2;
-let containerHeight = 640;
-
 const containerDiv = document.querySelector(".main-container");
-containerDiv.style.cssText = `height : ${containerHeight}px; width : ${containerHeight}px`;
+const clearButton = document.getElementById("clear");
+const nbOutput = document.getElementById("nb-output");
+let containerHeight = 500;
+// initialize the grid to 16 * 16 and allows to  paint
+addGrid(16);
+paint();
+showSize(16);
 
 
-let squareHeight = containerHeight / numberSide;
+// listen to the slider value changes and update the grid accordingly
+let slider = document.getElementById("nb");
+let num = 4;
+slider.addEventListener("input", () => {num = slider.value; changeSize(num)});
+function changeSize(n){
+    deleteGrid();
+    addGrid(n);
+    paint();
+    showSize(n)
+}
+
+// clear the board
+clearButton.addEventListener("click", clearBoard)
+function clearBoard(e) {
+    const square = document.querySelectorAll(".square");
+    square.forEach((element) => remClass(element));
+    function remClass(el) {
+        el.classList.remove("painted");      
+    }
+}
 
 
-for (let i = 0; i < numberSide; i++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("row-div");
-    rowDiv.style.cssText = `height : ${squareHeight}px; width : ${containerHeight}px;`;
-    containerDiv.appendChild(rowDiv);
+// logic
+function showSize(cells) {
+    nbOutput.textContent = `${cells}`;
+}
 
-    for (let j = 0; j < numberSide; j++) {
-        const square = document.createElement("div");
-        square.style.cssText = `height : ${squareHeight}px; width : ${squareHeight}px`;
-        rowDiv.appendChild(square);
+function paint() {
+    const square = document.querySelectorAll(".square");
+    console.log(square);
+    square.forEach((element) => listen(element));
+    function listen(el) {
+        el.addEventListener("mouseover", addClass)
+        function addClass(e) {
+            e.target.classList.add("painted");
+        }
+    }
+}
+
+function deleteGrid() {
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.lastChild)
+    }
+}
+
+function addGrid(cellPerSide) {
+    containerDiv.style.cssText = `height : ${containerHeight}px; width : ${containerHeight}px`;
+    let squareHeight = containerHeight / cellPerSide;
+    
+    for (let i = 0; i < cellPerSide; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row-div");
+        rowDiv.style.cssText = `height : ${squareHeight}px; width : ${containerHeight}px;`;
+        containerDiv.appendChild(rowDiv);
+    
+        for (let j = 0; j < cellPerSide; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.cssText = `height : ${squareHeight}px; width : ${squareHeight}px`;
+            rowDiv.appendChild(square);
+        }
     }
 }
